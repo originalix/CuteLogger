5397f8ea87652b3f55462e743398c1540ac2900f
Lix <xiao.liunit@gmail.com>
2017-06-24 21:46:07 +0800

    [LogStorage]: 获取日志缓存路径

--- CuteLogger/resource/LogStorage.swift
+++ CuteLogger/resource/LogStorage.swift
@@ -10,19 +10,28 @@ import UIKit
 
 protocol LogStorageProtocol {
     func getCachePath() -> String
-    func deleteFile(with name: String) -> Bool
-    func cleanCache() -> Bool
-    func readFile(with name: String) -> NSData?
-    func writeFile(with name:String, data: Data)
+//    func deleteFile(with name: String) -> Bool
+//    func cleanCache() -> Bool
+//    func readFile(with name: String) -> NSData?
+//    func writeFile(with name:String, data: Data)
     
 }
 
-class LogStorage: NSObject {
+class LogStorage: NSObject, LogStorageProtocol {
     
     let CACHEPATH = "LogStorage"
     
     private override init() { }
     
     static public let share: LogStorage = LogStorage()
+    
+    public func getCachePath() -> String {
+        var cacheDir = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
+        if (!cacheDir.hasSuffix("/")) {
+            cacheDir += "/"
+        }
+        
+        return cacheDir
+    }
     
 }
