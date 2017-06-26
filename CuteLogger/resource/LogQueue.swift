366f28d2c76295e5a85bf63f635a3e50bbe338f6
Lix <xiao.liunit@gmail.com>
2017-06-26 16:01:30 +0800

    [LogQueue]: Log日志队列数据结构完成

--- CuteLogger/resource/LogQueue.swift
+++ CuteLogger/resource/LogQueue.swift
@@ -8,22 +8,83 @@
 
 import UIKit
 
-struct LQueue {
-    var log: String
+struct LQueue<T> {
+    var logData = [T]()
     var front: Int
     var rear: Int
     var maxsize: Int;
 }
 
 protocol QueueProtocol {
-    func createQueue(queue: LQueue, maxsize: Int)
-    func traverseQueue(queue: LQueue)
-    func isFullQueue(queue: LQueue) -> Bool
-    func isEmptyQueue(queue: LQueue) -> Bool
-    func enqueue(queue: LQueue, log: String) -> Bool
-    func dequeue(queue: LQueue) -> Bool
+    func createQueue()
+    func traverseQueue()
+    func isFullQueue() -> Bool
+    func isEmptyQueue() -> Bool
+    func Enqueue(log: String) -> Bool
+    func Dequeue() -> Bool
 }
 
-class LogQueue: NSObject {
-
+class LogQueue: NSObject,  QueueProtocol {
+    static public let `default` = LogQueue()
+    private override init() {
+        super.init()
+        createQueue()
+    }
+    
+    var queue: LQueue<String>!
+    let maxsize: Int = 10
+    
+    func createQueue() {
+        queue = LQueue.init(logData: [String](), front: 0, rear: 0, maxsize: maxsize)
+    }
+    
+    func traverseQueue() {
+        var i = queue.front
+        print("队列中的元素是 : \n")
+        while (i % queue.maxsize != queue.rear) {
+            print("%d = %d \n", i, queue.logData[i])
+            i += 1
+        }
+    }
+    
+    func isFullQueue() -> Bool {
+        if (queue.front == (queue.rear + 1) % queue.maxsize) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func isEmptyQueue() -> Bool {
+        if (queue.front == queue.rear) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func getLength() -> Int {
+        return (queue.rear - queue.front + queue.maxsize) % queue.maxsize
+    }
+    
+    func Enqueue(log: String) -> Bool {
+        if isFullQueue() {
+            print("队列已满，插入失败 \n")
+            return false
+        }
+        
+        queue.logData[queue.rear] = log
+        queue.rear = (queue.rear + 1) % queue.maxsize
+        return true
+    }
+    
+    func Dequeue() -> Bool {
+        if isEmptyQueue() {
+            return false
+        }
+        
+        queue.front = (queue.front + 1) % queue.maxsize
+        return true
+    }
+    
 }
