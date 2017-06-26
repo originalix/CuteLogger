fb19bea9b00585cfb5e425f817b86e871f5ea689
Lix <xiao.liunit@gmail.com>
2017-06-26 16:11:14 +0800

    测试Swift类在Objc中的混编情况

---
+++ CuteLogger/Example/TestObjcViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestObjcViewController.h
+//  CuteLogger
+//
+//  Created by Lix on 2017/6/26.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestObjcViewController : UIViewController
+
+@end
---
+++ CuteLogger/Example/TestObjcViewController.m
@@ -0,0 +1,25 @@
+//
+//  TestObjcViewController.m
+//  CuteLogger
+//
+//  Created by Lix on 2017/6/26.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "TestObjcViewController.h"
+
+@interface TestObjcViewController ()
+
+@end
+
+@implementation TestObjcViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
